bcc880baf237ed330dc8ba9867e1382dcaad14bf
Arpita <arpita.20235019@mnnit.ac.in>
2026-06-29 13:32:40 +0530

    todays question added

---
+++ string/kmp.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numOfStrings(vector<string>& patterns, string word) {
+        unordered_map<string,int>mp;
+        int n=word.length();
+        for(int len=1;len<=n;len++){
+            for(int i=0;i+len<=n;i++){
+                mp[word.substr(i,len)]++;
+            }
+        }
+        int ans=0;
+        for(auto &wrd :patterns){
+            if(mp.count(wrd)){
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
