8e8548aa8716295b7086676a2ab229c8e3730c46
Arpita <arpita.20235019@mnnit.ac.in>
2026-06-30 13:55:00 +0530

    added daily question

--- easy/sliding window.cpp
+++ easy/sliding window.cpp
@@ -1,1 +1,24 @@
-cd 
+//lc-1358
+class Solution {
+public:
+    int numberOfSubstrings(string s) {
+        int n=s.length();
+        int cnt=0;
+        int i=0,j=0;
+        unordered_map<int,int>mp;
+        while(j<n){
+            mp[s[j]-'a']++;
+            while(i<n && mp.size()==3){
+                cnt+=(n-j);
+                mp[s[i]-'a']--;
+                if(mp[s[i]-'a']==0){
+                    mp.erase(s[i]-'a');
+                }
+                i++;
+            }
+            j++;
+        }
+        return cnt;
+    }
+};
+//standard two pointer approach
