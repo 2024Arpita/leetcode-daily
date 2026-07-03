4657e7c171be64477f6909a4e661e07a51dc2018
Arpita <arpita.20235019@mnnit.ac.in>
2026-07-03 12:35:28 +0530

    dijsktra+binary search added

---
+++ Graph/dijkstra+binary search on answer/lc3620.cpp
@@ -0,0 +1,48 @@
+//lc-3620
+class Solution {
+private:
+    long long check(int mid,vector<vector<pair<int,int>>>&adj,vector<bool>&online){
+        int n=online.size();
+        vector<long long>dis(n,LLONG_MAX);
+        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>>pq;
+        pq.push({0,0});
+        dis[0]=0;
+        while(!pq.empty()){
+            auto [w,node]=pq.top();pq.pop();
+            if(!online[node]) continue;
+            if(dis[node]<w) continue;
+            for(auto &it:adj[node]){
+                auto[neigh,ed]=it;
+                if(ed<mid || !online[neigh]) continue;
+                if(dis[neigh]>ed+w){
+                    dis[neigh]=ed+w;
+                    pq.push({dis[neigh],neigh});
+                }
+            }
+        }
+        return dis[n-1];
+    }
+public:
+    int findMaxPathScore(vector<vector<int>>& edges, vector<bool>& online,long long k) {
+        int n = online.size();
+        int s=INT_MAX,e=INT_MIN;
+        vector<vector<pair<int, int>>> adj(n);
+        for (auto& it : edges) {
+            int u = it[0], v = it[1], w = it[2];
+            adj[u].push_back({v, w});
+            s=min(s,w);
+            e=max(e,w);
+        }
+        int ans=-1;
+        while(s<=e){
+            int mid=s+(e-s)/2;
+            if(check(mid,adj,online)<=k){
+                ans=mid;
+                s=mid+1;
+            }else{
+                e=mid-1;
+            }
+        }
+        return ans;
+    }
+};
