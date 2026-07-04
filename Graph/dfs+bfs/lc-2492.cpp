06cabe8794dafcfd21bf4a1dd7ab2708a90a6111
Arpita <arpita.20235019@mnnit.ac.in>
2026-07-04 12:25:23 +0530

    added dsu aproach

--- Graph/dfs+bfs/lc-2492.cpp
+++ Graph/dfs+bfs/lc-2492.cpp
@@ -27,3 +27,55 @@ public:
 //we have to take minm edge weight in component having1 
 // for this we can do normal dfs and update edw wether that node is visited or not
 // since reviisting is alloweded
+//approach -2
+class dsu{
+  vector<int>par,size;
+  int n;
+  public:
+  dsu(int n){
+    par.resize(n+1);
+    size.resize(n+1,1);
+    for(int i=1;i<=n;i++){
+        par[i]=i;
+    }
+  }
+
+  int findp(int node){
+    if(node==par[node]) return node;
+    return par[node]=findp(par[node]);
+  }
+  
+  void unn(int u,int v){
+    int up=findp(u);
+    int vp=findp(v);
+    if(up==vp) return;
+    if(size[up]>size[vp]){
+        size[up]+=size[vp];
+        par[vp]=up;
+    }else{
+        size[vp]+=size[up];
+        par[up]=vp;
+    }
+  }
+
+};
+class Solution {
+public:
+    int minScore(int n, vector<vector<int>>& roads) {
+        int mini=INT_MAX;
+        dsu ds(n);
+        for(auto &it:roads){
+            int u=it[0],v=it[1],w=it[2];
+            ds.unn(u,v);
+        }
+        int p=ds.findp(1);
+        for(auto &it:roads){
+            int u=it[0],v=it[1],w=it[2];
+            if(ds.findp(u)==p){
+                mini=min(mini,w);
+            }
+        }
+        
+        return mini;
+    }
+};
