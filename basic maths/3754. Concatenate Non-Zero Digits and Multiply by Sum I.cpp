f6ae7e24e6beb0ab6592b3846b865dfa3fbe6ae4
Arpita <arpita.20235019@mnnit.ac.in>
2026-07-07 14:14:41 +0530

    explaination added

--- basic maths/3754. Concatenate Non-Zero Digits and Multiply by Sum I.cpp
+++ basic maths/3754. Concatenate Non-Zero Digits and Multiply by Sum I.cpp
@@ -1,1 +1,20 @@
 //lc easy
+class Solution {
+public:
+    long long sumAndMultiply(int n) {
+        int num = n;
+        int sum = 0;
+        int digsum = 0;
+        int place=1;
+        while (num) {
+            int dig = num % 10;
+            if (dig != 0) {
+                sum = sum+place*dig;
+                digsum += dig;
+                place*=10;
+            }
+            num /= 10;
+        }
+        return 1LL*sum*digsum;
+    }
+};
